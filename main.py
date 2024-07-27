b7129ef07099e1a0319559c4d06f551c8f078f47
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2024-07-26 23:27:19 -0400

    1

--- main.py
+++ main.py
@@ -3,11 +3,30 @@ from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
+from utils.api_requests import search_animes
+
 TOKEN: Final = environ.get("TOKEN", "")
 BOT_USERNAME: Final = environ.get("BOT_USERNAME", "")
 
+
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hola 👋... Bienvenido al descargador de anime gratis subtitulado al español")
+
+
+async def search_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Por favor, introduce el nombre de la serie de anime que deseas buscar.")
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    results = search_animes(user_message)
+
+    if results:
+        for anime in results:
+            caption = f"<b>{anime.title}</b>\n\n{anime.synopsis}"
+            await update.message.reply_photo(photo=anime.poster, caption=caption, parse_mode="HTML")
+    else:
+        await update.message.reply_text("No se encontraron resultados para tu búsqueda.")
 
 
 if __name__ == '__main__':
@@ -15,11 +34,12 @@ if __name__ == '__main__':
 
     # Commands
     app.add_handler(CommandHandler("start", start_command))
+    app.add_handler(CommandHandler("search", search_command))
 
     # Messages
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     # Errors
 
     # Polls the bot
     app.run_polling(poll_interval=3)
-
---
+++ utils/api_requests.py
@@ -0,0 +1,59 @@
+import time
+from typing import Any, List
+
+from cloudscraper.exceptions import CloudflareChallengeError
+
+from api.animeflv import AnimeInfo, AnimeFLV, EpisodeInfoDownload, EpisodeInfo, DownloadLinkInfo
+
+
+def wrap_request(func, *args, count: int = 10, expected: Any):
+    """
+    Wraps a request sent by the module to test if it works correctly, tries `count` times sleeps
+    5 seconds if an error is encountered.
+
+    If `CloudflareChallengeError` is encountered, the expected result will be returned
+    to make it possible for automated tests to pass
+
+    :param *args: args to call the function with.
+    :param count: amount of tries
+    :param expected: example for a valid return, this is used when cloudscraper complains
+    :rtype: Any
+    """
+    notes = []
+
+    for _ in range(count):
+        try:
+            res = func(*args)
+            if isinstance(res, list) and len(res) < 1:
+                raise ValueError()  # Raise ValueError to retry test when empty array is returned
+            return res
+        except CloudflareChallengeError:
+            return expected
+        except Exception as exc:
+            notes.append(exc)
+            time.sleep(5)
+    raise Exception(notes)
+
+
+def search_animes(search: str):
+    with AnimeFLV() as api:
+        data = wrap_request(api.search, search, expected=[AnimeInfo(0, "")])
+    return data
+
+def latest_animes():
+    with AnimeFLV() as api:
+        data = wrap_request(api.get_latest_animes, expected=[AnimeInfo(0, "")])
+    return data
+
+
+def get_anime_episode_info_download(id: str) -> List[EpisodeInfoDownload]:
+    with AnimeFLV() as api:
+        data: List[EpisodeInfo] = wrap_request(api.get_anime_info, id, expected=[AnimeInfo(0, "")]).episodes
+
+        r: List[EpisodeInfoDownload] = []
+
+        for e in data:
+            download = wrap_request(api.get_links, f'{e.anime}-{e.id}', expected=[List[DownloadLinkInfo('', '')]])
+            r.append(EpisodeInfoDownload(id=e.id, anime=e.anime, image_preview=e.image_preview, downloads=download))
+    return r
+
